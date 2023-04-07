ccc218cdd87b0e0e4baffe6c57c8027a96015187
Pranshu Singh Bisht <106018070+pranshubisht9@users.noreply.github.com>
2023-04-07 16:02:48 +0530

    correction

--- assets/js/ref.js
+++ assets/js/ref.js
@@ -41,7 +41,7 @@ let StarWarGithub = () => {
 }
 let myResume = () =>{
   window.open(
-    'https://drive.google.com/file/d/1Ow50dpmiVZnZsfaO0le8-8K9dsaHFiWO/view?usp=sharing',
+    'https://drive.google.com/file/d/1oQQeG4LNp_mRgWAiAjFMRhlIW3hsMXe2/view?usp=sharing',
     '_blank'
   );
 }
